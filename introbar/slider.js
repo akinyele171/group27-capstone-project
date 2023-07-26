7213038074e7986fd84421db23fa1b69cd9dc47c
akinyele171 <akinyeletemitopewilliams@gmail.com>
2023-07-26 15:08:38 +0100

    Second editing

---
+++ introbar/slider.js
@@ -0,0 +1,16 @@
+var img = document.getElementById('img');
+
+var slides = ['image 3.png', 'image 5.png', 'image 2.png'];
+
+var Start = 0;
+
+function slider() {
+    if(Start < slides.length) {
+        Start = Start + 1;
+    } else {
+        Start = 1
+    }
+    console.log(img);
+    img.innerHTML = "<img src=" + slides[Start - 1] + ">"
+}
+setInterval(slider, 2000);
